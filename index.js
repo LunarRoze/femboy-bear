cbc89a93f8834557086ef20e23010e4deb13498b
YTGWPAL <ytgwpal@gmail.com>
2022-01-11 15:33:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-module.exports = class Text extends Plugin {
-  startPlugin () {
-    this.registerCommand(
-      'text',
-      ['guide'],
-      'funny',
-      '{c}',
-      (args) => ({
+const { Plugin } = require("powercord/entities");
+
+module.exports = class PluginName extends Plugin {
+  startPlugin() {
+    powercord.api.commands.registerCommand({
+      command: 'femboybear',
+      description: 'funny',
+      usage: '{c} [ ...arguments ]',
+      executor: (args) => ({
         send: true,
         result: 'https://cdn.discordapp.com/attachments/525151751834304532/927690029785030726/femboy-bear.jpg'
       })
-    );
+    };)
   }
-};
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('femboybear');
+  }
+}
