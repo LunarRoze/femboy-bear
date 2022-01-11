bdd5dae24d96c019cf142090ede7baaa37f231a1
YTGWPAL <ytgwpal@gmail.com>
2022-01-11 16:37:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ module.exports = class PluginName extends Plugin {
         result: 'https://cdn.discordapp.com/attachments/525151751834304532/927690029785030726/femboy-bear.jpg'
       })
     });
-    powercord.api.commands.registerCommand({
-      command: 'dmbear',
-      description: 'funny 2',
-      usage: '{c} [ ...arguments ]',
-      executor: (args) => ({
-        const channelId = channels.getChannelId('670415208472248349');
-        
-        messages.sendMessage(channelId, { content: "https://cdn.discordapp.com/attachments/525151751834304532/927690029785030726/femboy-bear.jpg" });
-      })
-    });
   }
   pluginWillUnload() {
     powercord.api.commands.unregisterCommand('femboybear');
